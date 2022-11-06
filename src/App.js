85b13e07c60f42f87bf30dfd438a661916f8fb34
akashnai <akashnai632@gmail.com>
2022-11-06 18:35:57 +0530

    Lec2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <User name='akki' age='19' email='akki@akashnai.com' />
+      <User name='vivek' age='15' email='vivek@akashnai.com'  />
+      <User name='vaibhavi' age='18' email='vaibhavi@akashnai.com'  /> */}
+
+      <Job salary={90000} position='Senior SDE' company='Amazon' />
+      <Job salary={12000} position='Junior SDE' company='Google' />
+      <Job salary={10000} position='Project Manager' company='Netflix' />
     </div>
   );
 }
+
+const Job = ({salary, position, company}) => {
+  return (
+    <>
+      <h1>{salary}</h1>
+      <h1>{position}</h1>
+      <h1>{company}</h1>
+    </>
+  )
+}
+
+// const User = ({name, age, email}) => {
+//   return (
+//     <>
+//       <h1>{name}</h1>
+//       <h1>{age}</h1>
+//       <h1>{email}</h1>
+//     </>
+//   );
+// };
 
 export default App;
